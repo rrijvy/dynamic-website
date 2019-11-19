cd04ee77dfb446234a39f3195089f104dac00a42
Ashikur Rahman Rijvy <rrijvy@gmail.com>
2019-11-20 02:02:55 +0600

    a

---
+++ Alphasoft/Models/Software.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alphasoft.Models
+{
+    public class Software
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Image { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public int SoftwareCategoryId { get; set; }
+        public SoftwareCategory SoftwareCategory { get; set; }
+    }
+}
---
+++ Alphasoft/Models/SoftwareCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Alphasoft.Models
+{
+    public class SoftwareCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Thumbnail { get; set; }
+        public string Image { get; set; }
+    }
+}
