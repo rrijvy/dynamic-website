1e4bfe36eb75c20f027d0bb717e5eb2cb52a2797
mhanwar97@gmail.com <mhanwar97@gmail.com>
2019-11-19 05:43:47 -0800

    crude

--- Alphasoft/ViewModels/AboutUsViewModel.cs
+++ Alphasoft/ViewModels/AboutUsViewModel.cs
@@ -9,18 +9,11 @@ namespace Alphasoft.ViewModels
     {
         public int Id { get; set; }
         public string AboutMainSologan { get; set; }
-        public string AboutMainSologanDescription { get; set; }
-        public string OurMission { get; set; }
         public string OurMissionDescription { get; set; }
-        public string OurVission { get; set; }
-        public string OurVissionDescription { get; set; }
-        public string WhyUs { get; set; }
+        public string OurVisionDescription { get; set; }
         public string WhyUsDescription { get; set; }
-        //Who we are///
-        public string WhoWeAre { get; set; }
-        public string Description { get; set; }
-        public string ImageOne { get; set; }
-        public string ImageTwo { get; set; }
-        public string ImageThree { get; set; }
+        public string WhoWeAreDescription { get; set; }
+        public string WhoWeAreImageOne { get; set; }
+        public string WhoWeAreImageTwo { get; set; }
     }
 }
